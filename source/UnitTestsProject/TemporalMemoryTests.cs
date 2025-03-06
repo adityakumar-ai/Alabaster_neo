28235a01d5d44e09c139f9269eaba4a9d33cd42f
adityakumar-ai <chaurasia.k.aditya@gmail.com>
2025-03-06 23:44:29 +0100

    Testing Init 5 TestAddSegmentToCellWithFewestSegments

--- source/UnitTestsProject/TemporalMemoryTests.cs
+++ source/UnitTestsProject/TemporalMemoryTests.cs
@@ -738,6 +738,12 @@ namespace UnitTestsProject
                 TimeSpan elapsedParallel00 = stopwatch.Elapsed;
                 Console.WriteLine($"Time taken: {elapsedParallel00.TotalMilliseconds} milliseconds for Parallel Processing Init 4");
 
+                stopwatch.Start();
+                tmParallel.Init5(cn);
+                stopwatch.Stop();
+                TimeSpan elapsedParallel01 = stopwatch.Elapsed;
+                Console.WriteLine($"Time taken: {elapsedParallel01.TotalMilliseconds} milliseconds for Parallel Processing Init 5");
+
 
                 stopwatch.Start();
                 tmParallel.InitAsync(cn);
